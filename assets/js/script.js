5dd5e04b86773bbfda0439afc42f9dc9ccb29457
jesse sheehan <jessetsheehan@gmail.com>
2022-05-15 15:20:15 -0400

    added js functionality and mobile controls

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,3 +190,81 @@ $(".list-group").on("blur", "input[type='text']", function() {
 //replace input with span element
 $(this).replaceWith(taskSpan);
 });
+
+
+//draggable/droppable/sortable
+//turns every element withing .card .list-group sortable amongst other .card and .list groups
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("deactivate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  //loop over current set of children in a sortable list
+  //every time something is altered, this update method is called
+                                              //IMPORTANT FOR HOMEWORK?
+  update: function(event) {
+    var tempArr = [];
+
+    $(this).children().each(function(){
+      // a second $(this) within the callback function refers to a child element - these "this"s are two different things
+      
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      
+      var date = $(this)
+        .find("span")
+        .text() 
+        .trim(); 
+
+      // add task data to the temp array as an object:
+      tempArr.push({
+        text: text,
+        date: date
+      })
+
+      console.log(tempArr);
+
+    });
+
+    //trim down list's ID to match object property
+    //aka, finds the id and replaces it 
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();  
+
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
